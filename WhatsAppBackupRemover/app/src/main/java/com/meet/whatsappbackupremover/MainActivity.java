0d1f17bb001dcbbfdbff5331f875f89cc3bb8d15
Meet <meet.mukadam@somaiya.edu>
2017-11-11 09:16:34 +0530

    Partially Working

--- WhatsAppBackupRemover/app/src/main/java/com/meet/whatsappbackupremover/MainActivity.java
+++ WhatsAppBackupRemover/app/src/main/java/com/meet/whatsappbackupremover/MainActivity.java
@@ -1,7 +1,15 @@
 package com.meet.whatsappbackupremover;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton btn = (FloatingActionButton)findViewById(R.id.delete);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                File dir = new File("/Internal storage/WhatsApp/Databases/");
+                try {
+                    FileUtils.deleteDirectory(dir);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
